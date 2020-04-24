60180a529bae52e3132822ca265f98f43575bf25
ESPOIR <espoirditekemena@gmail.com>
2020-04-24 13:02:24 +0200

    my first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
+  templateUrl: './app.personal.html',
+  styleUrls: ['../assets/css/animate.css', '../assets/css/bootstrap.css', '../assets/css/icomoon.css', '../assets/css/flexslider.css', '../assets/css/owl.theme.default.min.css', '../assets/css/style.css', '../assets/css/owl.theme.default.min.css', '../assets/css/owl.carousel.min.css']
 })
 export class AppComponent {
   title = 'myapp';
